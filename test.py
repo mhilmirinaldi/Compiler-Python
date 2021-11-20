e4f2888abd65300ee8fff1f2925c3a2830b79bdb
Voguelish <voguelish7@gmail.com>
2021-11-20 13:49:44 +0700

    Komentar + Rapihin kode

--- test.py
+++ test.py
@@ -1,19 +1,26 @@
 # import sys
 
+# Meminta nama input file dan dijadikan sebagai suatu array
+# Return Array of Arrays
 def FileToArray():
     fileInput = open(str(input()), "r")
     fileInput = fileInput.readlines()
+
     prev = ''
-    
     codeArray = []
     for i in fileInput:
         length = len(i)
+
+        # Kalau line hanya berisi \n, akan di skip
         if (length == 1) & (i[0] == '\n'):
             continue
+
         if i[0] == ' ':
             startIndent = True
         else:
             startIndent = False
+
+        # Menghapus indentasi
         ctr = 0
         while (startIndent & (ctr < length)):
             if i[ctr] != ' ':
@@ -21,6 +28,7 @@ def FileToArray():
                 ctr -= 1
             ctr += 1
 
+        # Menulis ulang isi dari file serta menghapus spasi yang berlebihan
         codeLine = []
         for j in range(ctr, length):
             if (prev == ' ') & (i[j] != ' '):
@@ -42,6 +50,6 @@ print('\n========== Border 1 ==========\n')
 for i in A:
     for j in i:
         print (j, end='')
-    print()
+print()
 
 print("\nDone.")
