1e0d49fede745483c72be0116a0fbef733de4f46
mhilmirinaldi <aldi280402@gmail.com>
2021-11-24 17:43:32 +0700

    update tc bismillah terakhir

--- tc3rej.py
+++ tc3rej.py
@@ -1,12 +1,12 @@
-def iterate_something(x):
+def iterate_something():
     ''' Multiline comment
     '''
-    for i in range (x :
+    for i in range(3:
         print(i)
 
-iterate_something(3)
+iterate_something()
 
 i = int(input())
 while i <= 4:
-    print("Pengulangan ke-",i)
+    print("iterating")
     i += 1
